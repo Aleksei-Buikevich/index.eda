882437f36af7776facf3bee1faba1da3708ce8b0
Aleksei-Buikevich <67165224+Aleksei-Buikevich@users.noreply.github.com>
2021-10-25 14:31:18 +0300

    Add files via upload
    Написал CSS-правила для адаптивности сайта, как показано в уроке. Настроил слайдер на главном экране

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    prevEl: ".slider-button-prev",
+    nextEl: ".slider-button-next",
+  },
+
+});
